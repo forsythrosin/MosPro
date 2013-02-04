054f36ba76468eeb0e1d34aaec32fc876d31142b
emiax <mail@emilaxelsson.se>
2013-02-04 08:30:28 +0100

    plotting energy

--- prestudy/shapes/gravity.m
+++ prestudy/shapes/gravity.m
@@ -10,13 +10,15 @@ s1 = shape(2*[-1 -1 -1.2 1.2 0.1],2*[-1 1 0 0 1.1],'r');
 % s2.p = [-4 4]';
 % s2.theta = 10;
 
+s1.p(2) = 3.45;
+
 size = 11;
 
 totalEnergy = s1.getKineticEnergy() + s1.getPotentialEnergy(size);
 
 
 for t = 1:200000
-        
+    subplot(1, 2, 1);
     hold off;
     plot(size*[-1 -1 1 1 -1],size*[-1 1 1 -1 -1]);
     
@@ -30,6 +32,10 @@ for t = 1:200000
     
     totalEnergy = [totalEnergy s1.getKineticEnergy() + s1.getPotentialEnergy(size)];    
     
+     subplot(1, 2, 2);
+     hold off
+     plot(1:length(totalEnergy), totalEnergy);
+     
     
     pause(0.001);
 end
