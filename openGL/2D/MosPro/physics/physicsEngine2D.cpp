bcbba6de385a55b4c0f1eda786913e0b4ab778bd
emiax <mail@emilaxelsson.se>
2013-02-06 16:50:50 +0100

    gravity in openGL

--- openGL/2D/MosPro/physics/physicsEngine2D.cpp
+++ openGL/2D/MosPro/physics/physicsEngine2D.cpp
@@ -3,9 +3,28 @@
 
 PhysicsEngine2D::PhysicsEngine2D(void)
 {
+	bodies = std::vector<RigidBody2D*>();
 }
 
 
 PhysicsEngine2D::~PhysicsEngine2D(void)
 {
+}
+
+
+
+void PhysicsEngine2D::add(RigidBody2D* rb) {
+	bodies.push_back(rb);
+	rb->engine = this;
+}
+
+void PhysicsEngine2D::step() {
+	for(int i = 0; i < bodies.size(); i++) {
+		RigidBody2D* b = bodies[i];
+		b->step();
+	}
+}
+
+glm::vec2 PhysicsEngine2D::getGravity() {
+	return glm::vec2(0, -0.0001);
 }
--- openGL/2D/MosPro/physics/physicsEngine2D.h
+++ openGL/2D/MosPro/physics/physicsEngine2D.h
@@ -1,8 +1,24 @@
-#pragma once
+#ifndef __PHYSICSENGINE2D__
+#define __PHYSICSENGINE2D__
+#include "rigidBody2D.h"
+#include <vector>
+#include <glm/glm.hpp>
+
+class RigidBody2D;
+
 class PhysicsEngine2D
 {
-public:
-	PhysicsEngine2D(void);
-	~PhysicsEngine2D(void);
+	public:
+		PhysicsEngine2D(void);
+		~PhysicsEngine2D(void);
+		void add(RigidBody2D* rb);
+		void step();
+		glm::vec2 getGravity();
+
+	private:
+		std::vector<RigidBody2D*> bodies;
+
+	
 };
 
+#endif
